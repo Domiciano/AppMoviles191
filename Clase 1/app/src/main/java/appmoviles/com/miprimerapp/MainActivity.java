8c5aaa18a4621d41579f546c046be51979dee25a
Domi0620 <domiciano.rincon@correounivalle.edu.co>
2019-02-01 09:37:00 -0500

    Después de la clase 2

--- Clase 1/app/src/main/java/appmoviles/com/miprimerapp/MainActivity.java
+++ Clase 1/app/src/main/java/appmoviles/com/miprimerapp/MainActivity.java
@@ -1,7 +1,11 @@
 package appmoviles.com.miprimerapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
